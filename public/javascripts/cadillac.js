03cba03dfff8d631d8c1759dbba5f1eecbea7572
alohabeach <jackrahunziker@gmail.com>
2022-10-06 00:06:38 -1000

    Update cadillac.js

--- public/javascripts/cadillac.js
+++ public/javascripts/cadillac.js
@@ -1,6 +1,8 @@
+const container = document.getElementsByClassName('container1')[0];
 const btn = document.getElementsByClassName('btn')[0];
 
 btn.addEventListener('click', _ => {
+    container.remove();
     const vid = document.getElementById('vid');
     vid.style.display = '';
     vid.play();
